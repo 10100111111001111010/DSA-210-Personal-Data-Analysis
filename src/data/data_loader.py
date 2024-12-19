85e13dc837a3a84b2a733b44d6bdb43eacefcd08
nusret <ahmetnusretavci34@gmail.com>
2024-12-20 00:58:09 +0300

    data_loader.py implemented

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -1,1 +1,12 @@
+import urllib
+from bs4 import BeautifulSoup
+
+file_path = urllib.parse.unquote("/Users/nusret/Desktop/Sabancı Fall 2024-2025/DSA 210/Term Project/Takeout/YouTube and YouTube Music/history/watch-history.html")
+
+with open(file_path, "r") as file:
+    html_content = file.read()
+
+soup = BeautifulSoup(html_content, "lxml")
+
+print(soup.prettify())
 
